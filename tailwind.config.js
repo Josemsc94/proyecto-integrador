5f5049282ac9eaee8b3b9c725e1d6276a14c4bbd
Jose Miguel Sánchez Cano <josemiguelsancan@outlook.com>
2024-05-29 20:47:48 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "hardText": "#f97316",
+        "primaryText": "#fb923c",
+        "secundaryText": "#fdba74",
+        "smoothText": "##fed7aa",
+      },
+      screens: {
+      'iphone15': '390px',  // Breakpoint para iPhone 15
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
