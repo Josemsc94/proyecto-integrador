8ed42098fcb97962932ec8f511f72c5b93d41a08
Jose Miguel Sánchez Cano <josemiguelsancan@outlook.com>
2024-05-30 20:18:59 -0500

    Funcionalidad corregida de botones

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,9 @@
+const goup = document.getElementById('goup')
+const toggleOptionsButton = document.getElementById('toggle-options');
+const optionsMenu = document.getElementById('options-menu');
+const changeTheme = document.getElementById('change-theme');
+const body = document.body;
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -7,7 +13,24 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-document.getElementById('goup').onclick = function() {
+goup.onclick = function() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 };
+
+toggleOptionsButton.onclick = function() {
+    optionsMenu.classList.toggle('hidden');
+    optionsMenu.classList.toggle('visible');
+  };
+
+  changeTheme.onclick = function() {
+    if (body.classList.contains('bg-gray-800')) {
+      body.classList.remove('bg-gray-800', 'text-white');
+      body.classList.add('bg-white', 'text-gray-800');
+    } else {
+      body.classList.remove('bg-white', 'text-gray-800');
+      body.classList.add('bg-gray-800', 'text-white');
+    }
+    optionsMenu.classList.toggle('hidden');
+    optionsMenu.classList.toggle('visible');
+  };
   
